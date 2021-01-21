9def4f975301c0218d59773b879102dd3e6e952a
cagatay.cali <cagatay.cali@trendyol.com>
2021-01-22 02:41:59 +0300

    Graphql integration

--- src/app/components/project-card/project-card.js
+++ src/app/components/project-card/project-card.js
@@ -1,9 +1,8 @@
-const ProjectCard = ({ repoData }) => {
-  console.log(repoData);
+const ProjectCard = ({ repository }) => {
   return (
     <div className="p-3 bg-green-300 w-1/4 dark:bg-gray-900 bg-gray-100 m-2">
-      <div>Project name : {repoData.userName}</div>
-      <div>Homepage : {repoData.repoName}</div>
+      <div>Repository pathname: {repository.pathname}</div>
+      <div>Issue count: {repository.issues.length}</div>
     </div>
   );
 };
