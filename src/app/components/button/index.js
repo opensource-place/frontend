251ca566710ff62539eece0710b9b3ee40c78125
Altay Simsek <altaysimsek16@gmail.com>
2021-01-21 23:32:26 +0300

    Add button component

---
+++ src/app/components/button/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    border-radius: 8px;
+    padding: 0.5rem;
+    background-color: ${(props) => (props.primary ? "white" : "palevioletred")};
+`;
+
+export default StyledButton;
