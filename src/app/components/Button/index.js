7d108edfab98d3e422556962f56b8e05c2e0fe90
Damla <35731884+damlaehican@users.noreply.github.com>
2021-01-26 00:20:17 +0300

    :construction:the button component has been edited. Development will continue.

--- src/app/components/Button/index.js
+++ src/app/components/Button/index.js
@@ -1,8 +1,29 @@
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-    border-radius: 8px;
-    padding: 0.5rem;
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    border: ${(props) => props.border};
+    color: ${(props) => props.color};
+    border-radius: ${(props) => props.radius};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: ${(props) => props.fontSize};
+    background-color: ${(props) => (props.dublicate
+        ? '#FFC107'
+        : props.bug
+            ? '#B83239'
+            : props.issue
+                ? '#16B83F'
+                : props.wanted
+                    ? '#109DEB'
+                    : props.hard
+                        ? '#00463A'
+                        : props.easy
+                            ? '#008622'
+                            : '#00000')};
+    padding: 1rem;
     background-color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
 `
 
