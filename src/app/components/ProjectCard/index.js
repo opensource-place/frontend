691c68404bafe5eb39f88ab0e18105f9af8fd3a1
FurkanPortakal <furkanportakalx@gmail.com>
2021-01-26 22:32:20 +0300

    :fire: Add project card style
    Add project card style

--- src/app/components/ProjectCard/index.js
+++ src/app/components/ProjectCard/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Container, Text } from './style'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { Container, Text, topDiv } from './style'
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar'
 
 const ProjectCard = () => {
-  const value = 0.66
   return (
     <Container>
-      <Text>Hello Open Source!</Text>
-      <CircularProgressbar
-        value={value}
-        maxValue={1}
-        text={`${value * 100}%`}
-      />
+      <topDiv>
+        <div style={{ width: 112.98, height: 112.98 }}>
+          <CircularProgressbarWithChildren value={66}>
+            <div
+              style={{
+                fontSize: 12,
+                marginTop: -5,
+                display: 'flex',
+                justifyContent: 'center'
+              }}
+            >
+              <strong>66</strong> <br />
+              issues
+            </div>
+          </CircularProgressbarWithChildren>
+        </div>
+        <Text>open source place</Text>
+      </topDiv>
     </Container>
   )
 }
