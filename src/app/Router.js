eee6a3c60c0a68a398e0314680c1f0060c9cf76c
FurkanPortakal <furkanportakalx@gmail.com>
2021-01-30 15:31:07 +0300

    :fire: improved redux folder structures
    improved redux folder structures

--- src/app/Router.js
+++ src/app/Router.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import axios from 'axios'
+/*eslint-disable*/
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import axios from "axios";
 import {
   Projects,
   LandingPage,
   Start,
   Doc,
   Login,
-  ProjectDetail
-} from './pages'
-import { Provider } from 'react-redux'
-import store from './redux'
+  ProjectDetail,
+} from "./pages";
+import { Provider } from "react-redux";
+import storex from "./redux/store";
 
 const Router = () => {
-  axios.defaults.baseURL = process.env.REACT_APP_API_URL
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
   return (
-    <Provider store={store}>
+    <Provider store={storex}>
       <BrowserRouter>
-        <Route exact path='/' component={LandingPage} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/start' component={Start} />
-        <Route path='/doc' component={Doc} />
-        <Route path='/login' component={Login} />
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/start" component={Start} />
+        <Route path="/doc" component={Doc} />
+        <Route path="/login" component={Login} />
         <Route
-          path='/project/:projectname/:reponame'
+          path="/project/:projectname/:reponame"
           component={ProjectDetail}
         />
       </BrowserRouter>
-    </ Provider>
-  )
-}
+    </Provider>
+  );
+};
 
-export default Router
+export default Router;
--- src/app/pages/LandingPage.js
+++ src/app/pages/LandingPage.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from 'react'
-import { Footer, NavBar, StyledButton, ProjectCard } from '../components'
-import { Link } from 'react-router-dom'
-import Example from '../assets/example.svg'
-import axios from 'axios'
-import { useDispatch, useSelector } from 'react-redux'
-import { HeaderUp, DarkenBackground, AddSomeMargin, Invite, Center, Description, Container } from './style'
+/* eslint-disable */
+import React, { useEffect } from "react";
+import { Footer, NavBar, StyledButton, ProjectCard } from "../components";
+import { Link } from "react-router-dom";
+import Example from "../assets/example.svg";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  HeaderUp,
+  DarkenBackground,
+  AddSomeMargin,
+  Invite,
+  Center,
+  Description,
+  Container,
+} from "./style";
+import CONSTANTS from "../redux/actions";
 
 const LandingPage = () => {
-  const { projects } = useSelector((state) => state.projects)
-  const dispatch = useDispatch()
+  const { projects } = useSelector((state) => state.projects);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dataFetch()
-  }, [])
+    dataFetch();
+  }, []);
 
   const dataFetch = async () => {
     try {
@@ -23,30 +33,42 @@ const LandingPage = () => {
             title
           }
         }
-      }`
-      dispatch({ type: 'FETCHING_PROJECTS' })
-      const res = await axios.get(`http://localhost:8080/graphql?query=${query}`)
-      dispatch({ type: 'FETCHED_PROJECTS', payload: res.data.data.repositories })
+      }`;
+      dispatch({ type: CONSTANTS.FETHING_PROJECTS });
+      const res = await axios.get(
+        `http://localhost:8080/graphql?query=${query}`
+      );
+      dispatch({
+        type: CONSTANTS.FETCHED_PROJECTS,
+        payload: res.data.data.repositories,
+      });
     } catch (error) {
-      dispatch({ type: 'ERROR' })
+      dispatch({ type: CONSTANTS.ERROR });
     }
-  }
+  };
   return (
     <Container>
       <NavBar />
       <HeaderUp>
         <DarkenBackground>
           <AddSomeMargin>
-            <h2 style={{ color: '#444', fontWeight: 'bold' }}>
-              The platform includes open source projects. You can list all published
-              open-source projects and also add your project on Github to the
-              platform and your projects make open source!
+            <h2 style={{ color: "#444", fontWeight: "bold" }}>
+              The platform includes open source projects. You can list all
+              published open-source projects and also add your project on Github
+              to the platform and your projects make open source!
             </h2>
             <Invite>
               <Center>
                 <Link to="/start">
                   <a href="https://github.com/FurkanPortakal/opensourceadam">
-                    <StyledButton primary border="none" color="white" fontWeight="bold" padding="1.5rem" height="10px">
+                    <StyledButton
+                      primary
+                      border="none"
+                      color="white"
+                      fontWeight="bold"
+                      padding="1.5rem"
+                      height="10px"
+                    >
                       Feel free to contribute!
                     </StyledButton>
                   </a>
@@ -64,24 +86,47 @@ const LandingPage = () => {
         ))}
       </Center>
       <div style={{ marginRight: 480, marginLeft: 480 }}>
-        <div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start' }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: "flex-start",
+          }}
+        >
           <Description>
             <img width="200" height="200" src={Example} />
-            <p style={{ color: 'white' }}>Irure ad ipsum fugiat nisi ullamco ipsum ullamco anim non fugiat. Consectetur eu veniam sint esse commodo exercitation esse. Ex elit aliqua aliquip consequat eiusmod fugiat veniam labore id. Dolore eu voluptate Lorem eiusmod ex aliquip incididunt. Laborum enim adipisicing do anim esse excepteur.</p>
+            <p style={{ color: "white" }}>
+              Irure ad ipsum fugiat nisi ullamco ipsum ullamco anim non fugiat.
+              Consectetur eu veniam sint esse commodo exercitation esse. Ex elit
+              aliqua aliquip consequat eiusmod fugiat veniam labore id. Dolore
+              eu voluptate Lorem eiusmod ex aliquip incididunt. Laborum enim
+              adipisicing do anim esse excepteur.
+            </p>
           </Description>
         </div>
-        <div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-end' }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-start",
+            justifyContent: "flex-end",
+          }}
+        >
           <Description>
-            <p style={{ color: 'white' }}>Irure ad ipsum fugiat nisi ullamco ipsum ullamco anim non fugiat. Consectetur eu veniam sint esse commodo exercitation esse. Ex elit aliqua aliquip consequat eiusmod fugiat veniam labore id. Dolore eu voluptate Lorem eiusmod ex aliquip incididunt. Laborum enim adipisicing do anim esse excepteur.</p>
+            <p style={{ color: "white" }}>
+              Irure ad ipsum fugiat nisi ullamco ipsum ullamco anim non fugiat.
+              Consectetur eu veniam sint esse commodo exercitation esse. Ex elit
+              aliqua aliquip consequat eiusmod fugiat veniam labore id. Dolore
+              eu voluptate Lorem eiusmod ex aliquip incididunt. Laborum enim
+              adipisicing do anim esse excepteur.
+            </p>
             <img width="200" height="200" src={Example} />
-
           </Description>
         </div>
       </div>
 
       <Footer />
-    </Container >
-  )
-}
+    </Container>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
