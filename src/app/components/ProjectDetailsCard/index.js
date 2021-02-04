4d770a55cc8271600cfea8770617da08e1074991
FurkanPortakal <furkanportakalx@gmail.com>
2021-02-04 23:12:06 +0300

    :rocket: added get fork count and star count
    added get fork count and star count

--- src/app/components/ProjectDetailsCard/index.js
+++ src/app/components/ProjectDetailsCard/index.js
@@ -23,6 +23,7 @@ import Markdown from 'markdown-to-jsx'
 function ProjectDetail () {
   const { projectname, reponame } = useParams()
   const [issues, setIssues] = useState([])
+  const [repo, setRepo] = useState([])
   const [content, setContent] = useState('')
 
   useEffect(() => {
@@ -51,8 +52,24 @@ function ProjectDetail () {
       }
     }`
 
+    const repos = `{
+    repository(pathname:"/${projectname}/${reponame}"){
+      pathname
+      forks_count
+      stargazers_count
+      contributors{
+        avatar_url
+      }
+     }
+    }`
+
     const res = await axios.get(`http://localhost:8080/graphql?query=${query}`)
     setIssues(res.data.data.issues)
+
+    const repox = await axios.get(
+      `http://localhost:8080/graphql?query=${repos}`
+    )
+    setRepo(repox.data.data.repository)
   }
 
   const getMarkdown = async () => {
@@ -61,7 +78,7 @@ function ProjectDetail () {
     )
     setContent(await res.data)
   }
-  console.log(content)
+
   return (
     <Container>
       <Details>
@@ -79,11 +96,11 @@ function ProjectDetail () {
           </DetailsRightRow>
           <DetailsRightRow>
             <img src={Star} />
-            <DetailsRightText>78</DetailsRightText>
+            <DetailsRightText>{repo.stargazers_count}</DetailsRightText>
           </DetailsRightRow>
           <DetailsRightRow>
             <img src={Fork} />
-            <DetailsRightText>78</DetailsRightText>
+            <DetailsRightText>{repo.forks_count}</DetailsRightText>
           </DetailsRightRow>
         </DetailsRight>
       </Details>
@@ -112,24 +129,14 @@ function ProjectDetail () {
         </Readme>
         <Readme>
           <h1>Where does it come from?</h1>
-          <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 ofde Finibus Bonorum et Malorum (The Extremes of
-            Good and Evil) by Cicero, written in 45 BC. This book is a treatise
-            on the theory of ethics, very popular during the Renaissance. The
-            first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from
-            a line in section 1.10.32. The standard chunk of Lorem Ipsum used
-            since the 1500s is reproduced below for those interested. Sections
-            1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are
-            also reproduced in their exact original form, accompanied by English
-            versions from the 1914 translation by H. Rackham.
-          </p>
+          {issues.map((item, i) => (
+            <IssuesDetail key={i}>
+              <a href={item.html_url}>
+                <Text>{item.title}</Text>
+              </a>
+              <Text>opened {findDate(item.created_at)} days ago</Text>
+            </IssuesDetail>
+          ))}
         </Readme>
       </BottomDiv>
     </Container>
